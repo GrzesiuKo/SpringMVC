4d244a6fe174dde171a64cc7fecbf13b1a96913b
GrzesiuKo <gak@autograf.pl>
2019-04-05 00:21:34 +0200

    Added first REST API

--- src/main/java/com/pluralsight/controller/MinutesController.java
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -1,9 +1,15 @@
 package com.pluralsight.controller;
 
+import com.pluralsight.model.Activity;
 import com.pluralsight.model.Exercise;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MinutesController {
@@ -14,6 +20,27 @@ public class MinutesController {
 
         return "addMinutes";
     }
+
+    @RequestMapping(value = "/activities", method = RequestMethod.GET)
+    public @ResponseBody List<Activity> findAllActivities(){
+        List<Activity> activities = new ArrayList<>();
+        Activity run = new Activity();
+        run.setDesc("Run");
+        activities.add(run);
+
+        Activity bike = new Activity();
+        bike.setDesc("bike");
+        activities.add(bike);
+
+        Activity swim = new Activity();
+        swim.setDesc("swim");
+        activities.add(swim);
+
+        return activities;
+    }
+
+
+
 //    @RequestMapping(value = "/addMoreMinutes")
 //    public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise){
 //
